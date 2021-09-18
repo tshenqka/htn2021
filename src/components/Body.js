0af653bfd4e0ccb01f7058294cbfe612fac512ea
Victor Sun <victorjsun@gmail.com>
2021-09-18 19:16:05 -0400

    Add multiple songs

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,13 +7,15 @@ import Song from './Song';
 import coda from '../coda'
 
 function Body() {
-    const [song, setSong] = React.useState(null);
+    const [songs, setSongs] = React.useState([]);
 
     useEffect(() => {
         coda.getDoc('19Z2_he_i3').then(doc => doc.getTable('grid-sS3AhHHpH1').then(table => table.listRows({useColumnNames: true}).then(res => {
             res.forEach(row => {
-                setSong({id: row.id,
-                notes: row.values.Notes})
+                var oldSongs = songs;
+                oldSongs.push({id: row.id,
+                    notes: row.values.Notes})
+                setSongs(oldSongs)
             })
         })))
     }, [])
@@ -22,8 +24,8 @@ function Body() {
         <ParallaxProvider>
             <Title playlistName="Best Playlist Ever" creatorName="Wolgang Amadeus Mozart"></Title>
             <Stars/>
-            {song && (
-                <Song x={100} y={650*} iconSize={200} noteSize={300} songName="Willow" artistName="Taylor Swift" albumArtLink="https://media.pitchfork.com/photos/5f1e2abad421092dd8f6c7ca/1:1/w_320/Taylor_Swift_folklore.jpeg" rowId={song.id} initialNotes={song.notes}/>
+            {songs.length != 0 && (
+                songs.map((song, ind) => <Song x={100} y={650*(ind+1)} iconSize={200} noteSize={300} songName="Willow" artistName="Taylor Swift" albumArtLink="https://media.pitchfork.com/photos/5f1e2abad421092dd8f6c7ca/1:1/w_320/Taylor_Swift_folklore.jpeg" rowId={song.id} initialNotes={song.notes}/>)
             )}
             {/* <Song x={100} y={650} iconSize={200} noteSize={300} songName="Willow" artistName="Taylor Swift" albumArtLink="https://media.pitchfork.com/photos/5f1e2abad421092dd8f6c7ca/1:1/w_320/Taylor_Swift_folklore.jpeg"/>
             <Song x={600} y={1300} iconSize={200} noteSize={300} songName="Jail" artistName="Kanye West" albumArtLink="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/A_black_background.jpg/640px-A_black_background.jpg"/>
