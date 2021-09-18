bde7856ed6042147f6a3766500c6810acd506329
Julia Zhang <juliazhang1108@gmail.com>
2021-09-18 10:49:21 -0400

    add icon to title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,12 +3,14 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import '@fontsource/roboto/300.css';
 import { Parallax } from 'react-scroll-parallax';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
 function Title(props) {
     const { playlistName, creatorName } = props;
     return (
-        <div style={{paddingTop: '25%'}}>
+        <div style={{paddingTop: '20%'}}>
             <Parallax y={[-20, 20]}>
+                <MusicNoteIcon sx={{ fontSize: '50px', color: '#f4cb85'}}/>
                 <Typography component="div">
                     <Box sx={{ typography: 'h2', color: '#f4cb85'}}>
                         { playlistName }
