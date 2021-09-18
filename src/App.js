7e5b191e383b99a833bdef18a8ffb3d567ed1a0e
Julia Zhang <juliazhang1108@gmail.com>
2021-09-18 10:43:22 -0400

    title and background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Stars from './components/Stars/Stars';
+import Title from './components/Title/Title';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <ParallaxProvider>
+        <Title playlistName="Best Playlist Ever" creatorName="Wolgang Amadeus Mozart"></Title>
+        <Stars/>
+      </ParallaxProvider>
     </div>
   );
 }
